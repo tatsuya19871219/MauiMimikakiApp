ede42b6b5506c361f823c06ef3cb576c98492766
Tatsuya Yamada <tatsuya19871219@gmail.com>
2023-04-20 08:43:41 +0900

    Rename folders

--- MauiMimikakiApp/MimiModels/ITrackerListener.cs
+++ MauiMimikakiApp/MimiModels/ITrackerListener.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using TakeMauiEasy;
 
 namespace MauiMimikakiApp;
+
 internal interface ITrackerListener
 {
     void OnMove(Point position, Point velocity, double milliSecUpdateInterval);
